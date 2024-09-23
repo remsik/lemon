87aee00e9e22b6faae77655c598f9b000d5716de
Zak Remsik <32900194+remsik@users.noreply.github.com>
2024-09-23 23:29:19 +0100

    LEMON-005 added trusted host

--- littleLemon/settings.py
+++ littleLemon/settings.py
@@ -129,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['http://35.197.217.120']
